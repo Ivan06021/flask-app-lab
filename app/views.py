7782d1ee01c4e9ace9f04e4dc061221c345cef11
Ivan006021 <ivan.kostiuk.22@pnu.edu.ua>
2024-12-02 18:38:46 +0200

    Add user bp

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, abort
+from flask import render_template, request
 from . import app
 
 
@@ -12,20 +12,6 @@ def home():
     """View foe the Home page of your website"""
     agent = request.user_agent
     return render_template("home.html", agent=agent)
-
-
-@app.route("/hi/<string:name>")
-def greeting(name):
-    name = name.upper()
-    age = request.args.get("age", 0, int)
-    return render_template("hi.html", name=name, age=age)
-
-
-@app.route("/admin")
-def admin():
-    to_url = url_for("greeting", name="administrator", age=30, _external=True)
-    print(to_url)
-    return redirect(to_url)
 
 
 @app.route("/resume")

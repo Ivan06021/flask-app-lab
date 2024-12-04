52c9322594b27a223901324994f5527fb460f39e
Ivan006021 <ivan.kostiuk.22@pnu.edu.ua>
2024-12-05 01:11:08 +0200

    Add post management features: create post form, templates, and views (task1)

--- app/posts/views.py
+++ app/posts/views.py
@@ -1,36 +1,54 @@
-from flask import render_template, abort
+import os
+from flask import json, render_template, abort, redirect, flash, session, url_for
 from . import post_bp
+from .forms import PostForm
 
-posts = [
-    {
-        "id": 1,
-        "title": "My First Post",
-        "content": "This is the content of my first post.",
-        "author": "John Doe",
-    },
-    {
-        "id": 2,
-        "title": "Another Day",
-        "content": "Today I learned about Flask macros.",
-        "author": "Jane Smith",
-    },
-    {
-        "id": 3,
-        "title": "Flask and Jinja2",
-        "content": "Jinja2 is powerful for templating.",
-        "author": "Mike Lee",
-    },
-]
+DB = "posts.json"
+
+
+def load_posts():
+    if not os.path.exists(DB):
+        return []
+    with open(DB, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def save_posts(posts):
+    with open(DB, "w", encoding="utf-8") as file:
+        json.dump(posts, file, indent=4)
 
 
 @post_bp.route("/")
 def get_posts():
+    posts = load_posts()
     return render_template("posts/posts.html", posts=posts)
 
 
 @post_bp.route("/<int:id>")
 def get_post(id):
-    if id > len(posts):
+    posts = load_posts()
+    if id > len(posts) or id <= 0:  # Перевіряємо, чи існує пост
         abort(404)
     post = posts[id - 1]
     return render_template("posts/detail-post.html", post=post)
+
+
+@post_bp.route("/add_post", methods=["GET", "POST"])
+def add_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        posts = load_posts()
+        new_post = {
+            "id": len(posts) + 1,
+            "title": form.title.data,
+            "content": form.content.data,
+            "is_active": form.is_active.data,
+            "publish_date": form.publish_date.data.strftime("%Y-%m-%d"),
+            "category": form.category.data,
+            "author": session.get("username", "Anonymous"),
+        }
+        posts.append(new_post)
+        save_posts(posts)
+        flash("Post added successfully", "success")
+        return redirect(url_for("posts.add_post"))
+    return render_template("posts/add_post.html", form=form)
